5ba6035d003a7fb54b7899ca941ae94df265fe36
Rainer Telesko <rainer.telesko@gmail.com>
2022-03-25 17:15:49 +0100

    Remove unnecessary casts, change accelerator type to linear and and adapt minimum API level for using Streams

--- BeerAdviser/src/main/java/com/hfad/beeradviser/FindBeerActivity.java
+++ BeerAdviser/src/main/java/com/hfad/beeradviser/FindBeerActivity.java
@@ -20,10 +20,10 @@ public class FindBeerActivity extends Activity {
     //Call when the button gets clicked
     public void onClickFindBeer(View view) {
         //Get a reference to the TextView
-        TextView brands = (TextView) findViewById(R.id.brands);
+        TextView brands = findViewById(R.id.brands);
 
         //Get a reference to the Spinner
-        Spinner color = (Spinner) findViewById(R.id.color);
+        Spinner color = findViewById(R.id.color);
 
         //Get the selected item in the Spinner
         String beerType = String.valueOf(color.getSelectedItem());
